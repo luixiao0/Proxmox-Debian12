d82455f6c05b4e2796afbc29f6189f82fe11135b
lux <lux@luxhub.top>
2024-11-05 15:24:56 +0800

    refactor: update Proxmox repository URL to use Tsinghua mirror in install script.

--- scripts/install_proxmox-1.sh
+++ scripts/install_proxmox-1.sh
@@ -231,7 +231,7 @@ install_proxmox-2()
         echo -e "...${default}"
     fi
 
-    echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+    echo "deb [arch=amd64] https://mirrors.tuna.tsinghua.edu.cn/proxmox/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
 
     wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
 
